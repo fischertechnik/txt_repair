6312937f948e3fbbda8b3ab7565768e84ff29f93
Alexander Steiger <42381763+AlexanderSteiger@users.noreply.github.com>
2018-09-04 17:31:42 +0200

    Update

--- 0_new_root.bat
+++ 0_new_root.bat
@@ -1,7 +1,8 @@
 echo off
 
-Utilities\plink -pw ROBOPro ROBOPro@192.168.7.2 sudo /usr/sbin/new_root_password.sh 60
-
+echo ----------------------------------------------------
+echo Wait until the root password is displayed on TXT ...
 echo Write down the passwort ...
+echo ----------------------------------------------------
 
-pause
+Utilities\plink -pw ROBOPro ROBOPro@192.168.7.2 sudo /usr/sbin/new_root_password.sh 60
